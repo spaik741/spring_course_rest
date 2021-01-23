9dd3aca45cc51a1a1d28530a391046d3b90c0bdc
bryahchin.mihail <bryahchin.mihail@otr.ru>
2021-01-23 14:07:52 +0300

    exception handler

--- src/main/java/com/mihail/spring/rest/controller/MyRestController.java
+++ src/main/java/com/mihail/spring/rest/controller/MyRestController.java
@@ -1,13 +1,16 @@
 package com.mihail.spring.rest.controller;
 
 import com.mihail.spring.rest.entity.Employee;
+import com.mihail.spring.rest.exception_handling.EmployeeIncorrectData;
 import com.mihail.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/api")
@@ -20,5 +23,14 @@ public class MyRestController {
     public List<Employee> showAllEmployees() {
         List<Employee> allEmployees = employeeService.getAllEmployees();
         return allEmployees;
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee showEmployee(@PathVariable int id) {
+        Employee employee = employeeService.getEmployee(id);
+        if(employee==null){
+            throw new NoSuchElementException("Employee with id = " + id + " not found.");
+        }
+        return employee;
     }
 }
---
+++ src/main/java/com/mihail/spring/rest/exception_handling/EmployeeGlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.mihail.spring.rest.exception_handling;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.NoSuchElementException;
+
+@ControllerAdvice
+public class EmployeeGlobalExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handlerException(NoSuchElementException exception){
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handlerException(Exception exception){
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
+    }
+}
